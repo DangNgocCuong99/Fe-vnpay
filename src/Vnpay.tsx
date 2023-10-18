666115613830b44d3b2e653f4b6e38bbb8a382ae
Henry <92662668+DangNgocCuong99@users.noreply.github.com>
2023-10-18 11:30:27 +0700

    1

--- src/Vnpay.tsx
+++ src/Vnpay.tsx
@@ -17,7 +17,7 @@ const VNPayPayment = () => {
     //   const data = await response.json();
 
       // Khi nhận được dữ liệu từ server, chuyển hướng người dùng đến trang thanh toán VNPay
-      window.location.href = "https://sandbox.vnpayment.vn/paymentv2/Transaction/PaymentMethod.html?token=498622783b5e4976b1f4ac22465b4fe3";
+      window.location.href = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?vnp_Amount=1000000&vnp_Command=pay&vnp_CreateDate=20231018112731&vnp_CurrCode=VND&vnp_IpAddr=%3A%3A1&vnp_Locale=vn&vnp_OrderInfo=Thanh+toan+cho+ma+GD%3A18112731&vnp_OrderType=other&vnp_ReturnUrl=http%3A%2F%2Flocalhost%3A8888%2Forder%2Fvnpay_return&vnp_TmnCode=3LL1IASJ&vnp_TxnRef=18112731&vnp_Version=2.1.0&vnp_SecureHash=49ace7905a8cbbbcc6ac7f58d39254717ece4d3fcfbe43dd0f338099910f9170ea6740dd1352609d74e215b9983ca73147fcbdbf1c09049ee27647c8820fe411";
     } catch (error) {
       console.log(error);
     }
